fc9eb9295e6808b376b8ce3e9a571f4e6960633f
daonq2001 <daonq.nde19046@vtc.edu.vn>
2020-09-06 19:57:26 +0700

    Set up hinh anh

---
+++ Mario Bino Go/Assets/Scripts/GameManager.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public enum stateGame { PAUSE, DIE, WIN, PLAY }
+    public stateGame status;
+
+    [SerializeField] private Button pauseBtn;
+    [SerializeField] private Button leftBtn;
+    [SerializeField] private Button rightBtn;
+    [SerializeField] private Button jumpBtn;
+
+    [SerializeField] private GameObject pannelWin;
+    [SerializeField] private GameObject pannelPause;
+    [SerializeField] private GameObject pannelDied;
+
+    [SerializeField] private Text textScore;
+    [SerializeField] private Text textTime;
+    [SerializeField] private Text textCoin;
+
+    [SerializeField] private int score;
+    [SerializeField] private int time;
+    [SerializeField] private int coin;
+    
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        switch (status)
+        {
+            case stateGame.PAUSE:
+                Pause();
+                break;
+            case stateGame.DIE:
+                Die();
+                break;
+            case stateGame.WIN:
+                Win();
+                break;
+            case stateGame.PLAY:
+                Play();
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+
+        pannelPause.SetActive(true);
+        pannelDied.SetActive(false);
+        pannelWin.SetActive(false);
+
+        textCoin.gameObject.SetActive(false);
+        textTime.gameObject.SetActive(false);
+        textScore.gameObject.SetActive(false);
+
+        pauseBtn.gameObject.SetActive(false);
+        leftBtn.gameObject.SetActive(false);
+        rightBtn.gameObject.SetActive(false);
+        jumpBtn.gameObject.SetActive(false);
+    }
+
+    private void Die()
+    {
+        time = 400;
+        Time.timeScale = 1f;
+        pannelPause.SetActive(false);
+        pannelDied.SetActive(true);
+        pannelWin.SetActive(false);
+
+        textCoin.gameObject.SetActive(false);
+        textTime.gameObject.SetActive(false);
+        textScore.gameObject.SetActive(false);
+
+        pauseBtn.gameObject.SetActive(false);
+        leftBtn.gameObject.SetActive(false);
+        rightBtn.gameObject.SetActive(false);
+        jumpBtn.gameObject.SetActive(false);
+    }
+
+    private void Win()
+    {
+        time = 400;
+        Time.timeScale = 1f;
+        pannelPause.SetActive(false);
+        pannelDied.SetActive(false);
+        pannelWin.SetActive(true);
+
+        textCoin.gameObject.SetActive(false);
+        textTime.gameObject.SetActive(false);
+        textScore.gameObject.SetActive(false);
+
+        pauseBtn.gameObject.SetActive(false);
+        leftBtn.gameObject.SetActive(false);
+        rightBtn.gameObject.SetActive(false);
+        jumpBtn.gameObject.SetActive(false);
+    }
+
+    private void Play()
+    {
+        time -= Mathf.RoundToInt(Time.deltaTime);
+        Time.timeScale = 1f;
+        pannelPause.SetActive(false);
+        pannelDied.SetActive(false);
+        pannelWin.SetActive(false);
+
+        textCoin.gameObject.SetActive(true);
+        textTime.gameObject.SetActive(true);
+        textScore.gameObject.SetActive(true);
+
+        pauseBtn.gameObject.SetActive(true);
+        leftBtn.gameObject.SetActive(true);
+        rightBtn.gameObject.SetActive(true);
+        jumpBtn.gameObject.SetActive(true);
+
+        textCoin.text = "Coins: " + "\n" + coin;
+        textScore.text = "Score: " + "\n" + score;
+        textTime.text = "Time: " + "\n" + time;
+    }
+
+
+}
---
+++ Mario Bino Go/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool Left, Right, Jump;
+    private bool onGround;
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float maxSpeed = 3f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Left)
+        {
+            rb.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
+            if(rb.velocity.x < -maxSpeed)
+            {
+                rb.velocity = new Vector2(-maxSpeed, rb.position.y);
+            }
+            anim.SetFloat("Speed", 1f);
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        } else if (Right)
+        {
+            rb.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+            if (rb.velocity.x > maxSpeed)
+            {
+                rb.velocity = new Vector2(maxSpeed, rb.position.y);
+            }
+            anim.SetFloat("Speed", 1f);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        } else if (Jump && onGround)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("Jump", true);
+            onGround = false;
+        } else if(Left && Jump && onGround)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            rb.AddForce(new Vector2(-1f, -1f) * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("Jump", true);
+            onGround = false;
+        }
+        else if (Right && Jump && onGround)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            rb.AddForce(new Vector2(1f, 1f) * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("Jump", true);
+            onGround = false;
+        }
+    }
+
+    public void LeftDown()
+    {
+        Left = true;
+    }
+
+    public void LeftUp()
+    {
+        Left = false;
+    }
+
+    public void RightDown()
+    {
+        Right = true;
+    }
+
+    public void RighUp()
+    {
+        Right = false;
+    }
+
+    public void JumpDown()
+    {
+        rb.gravityScale = 0f;
+        Jump = true;
+        StartCoroutine(delayJumpDown());
+    }
+
+    IEnumerator delayJumpDown()
+    {
+        yield return new WaitForSeconds(0.4f);
+        rb.gravityScale = 5f;
+        Jump = false;
+    }
+
+    public void JumpUp()
+    {
+        Jump = false;
+    }
+
+    public void Pause()
+    {
+        GameManager.Instance.status = GameManager.stateGame.PAUSE;
+    }
+
+    public void Continue()
+    {
+        GameManager.Instance.status = GameManager.stateGame.PLAY;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            GameManager.Instance.status = GameManager.stateGame.WIN;
+        } else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            anim.SetBool("Died", true);
+            GameManager.Instance.status = GameManager.stateGame.DIE;
+        } else if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+}
